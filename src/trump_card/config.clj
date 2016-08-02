12c30f5725749643dcae45a9fd0a79e2dc42270e
William Rudenmalm <me@whn.se>
2016-08-02 21:31:21 +0200

    Warn when missing config file

--- src/trump_card/config.clj
+++ src/trump_card/config.clj
@@ -1,10 +1,14 @@
 (ns trump-card.config
   (:require [aero.core :as aero]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [taoensso.timbre :refer [warn]]))
 
 (def config
   (if-let [p (io/resource "config.edn")]
-    (aero/read-config p {})))
+    (aero/read-config p {})
+    (do
+      (warn "Missing config file `config.edn`")
+      {})))
 
 (defn merge-with-config [x]
   (merge-with merge config x))
